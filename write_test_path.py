ba1bfc529a4f4244f11b565627a301c2ab2d8c70
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2020-04-16 10:10:33 +0200

    Rename model bvh dir.

--- write_test_path.py
+++ write_test_path.py
@@ -131,9 +131,9 @@ def convert_bvh(fn_in, fn_out='path.txt'):
 if __name__ == '__main__':
     from glob import glob
 
-    # for fn in glob('bvh/*.bvh'):
-    #     convert_bvh(fn, fn.split('.')[0]+'.txt')
-    for fn in glob('bvh/generated/*/*.bvh'):
+    for fn in glob('bvh/*.bvh'):
+        convert_bvh(fn, fn.split('.')[0]+'.txt')
+    for fn in glob('bvh/vr_prediction_models/*/*.bvh'):
         convert_bvh(fn, fn.split('.')[0]+'.txt')
 
     # import matplotlib.pyplot as plt
