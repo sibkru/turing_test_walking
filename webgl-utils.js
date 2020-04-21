36d4657a550bc1679ad2b58348d4cf24e87a344f
Benjamin Knopp <benjamin.knopp@uni-marburg.de>
2020-04-21 11:09:00 +0200

    Switch to glPoint primitives.

--- webgl-utils.js
+++ webgl-utils.js
@@ -48,3 +48,62 @@ function loadFile(filePath) {
     }
     return result;
 }
+
+function createTrajectory(trial) {
+    var train = loadFile(trial.fn_train).split('\n');
+    path1 = train
+    console.log(trial)
+    if (trial.natural){
+        var path2 = train;
+    } else {
+        var model = loadFile(trial.fn).split('\n');
+        var path2 = model;
+    }
+
+    const dofs = path1[0].split(";").length
+    var lines = [];
+    for (var j = 0; j < path1.length-1; ++j){
+        if (j < trial.occ_start) {
+            lines.push(path1[j].split(";").map(parseFloat));
+        } else if (j > trial.occ_end) {
+	    if (!path2[j]) {
+	        debugger;
+	    }
+            lines.push(path2[j].split(";").map(parseFloat));
+        } else {
+            lines.push(new Array(dofs).fill(0.0));
+        }
+    }
+    return lines
+}
+
+function setupCanvas() {
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('id', 'glcanvas');
+    canvas.width = 640;
+    canvas.height = 480;
+    canvas.setAttribute("z-index", 9999);
+    document.getElementById('jspsych-content').appendChild(canvas);
+}
+
+function resize(canvas) {
+    // Lookup the size the browser is displaying the canvas.
+    var displayWidth  = canvas.clientWidth;
+    var displayHeight = canvas.clientHeight;
+
+    // Check if the canvas is not the same size.
+    if (canvas.width  != displayWidth ||
+      canvas.height != displayHeight) {
+
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+    }
+}
+
+function minimizeCanvas() {
+    const canvas = document.getElementById('glcanvas')
+    canvas.width = 40;
+    canvas.height = 80;
+}
+
