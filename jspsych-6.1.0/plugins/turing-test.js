6aadab7aa610e56adb4d43da53258a8f036c7e23
Benjamin Knopp <knoppbe@staff.uni-marburg.de>
2020-07-03 11:27:17 +0200

    Test multiple canvas presentation.

---
+++ jspsych-6.1.0/plugins/turing-test.js
@@ -0,0 +1,34 @@
+jsPsych.plugins['turing-test'] = (function() {
+    var plugin = {};
+
+    plugin.info = {
+        name: 'turing-test',
+        parameters: {
+            stimulus: {
+                type: jsPsych.plugins.parameterType.STRING,
+                pretty_name: 'filename',
+                default: null,
+                description: 'filename.'
+            }
+        },
+        choices: {
+            type: jsPsych.plugins.parameterType.KEYCODE,
+            pretty_name: 'Choices',
+            array: true,
+            default: jsPsych.ALL_KEYS,
+            description: 'The keys the subject is allowed to press to respond to the stimulus.'
+        },
+    }
+
+    plugin.trial = function(display_element, trial){
+        var train = loadFile('bvh/pass-bottle-hold-training0-lines.txt').split('\n');
+        var T = train.length / 24 * 1000
+        main('#glcanvas_r');
+        main('#glcanvas_l');
+        jsPsych.pluginAPI.setTimeout(function() {
+            jsPsych.finishTrial();
+        }, T);
+    }
+
+    return plugin;
+})();
