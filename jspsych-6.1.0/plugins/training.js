a76649f7a69044cc60d9c6f2260638ed8956a365
Hieu <splnkr@disroot.org>
2020-09-08 16:32:24 +0200

    same framenumber

--- jspsych-6.1.0/plugins/training.js
+++ jspsych-6.1.0/plugins/training.js
@@ -23,8 +23,20 @@ jsPsych.plugins['training'] = (function() {
     plugin.trial = function(display_element, trial){
         var T = Math.max(trainLeft.length, trainRight.length) / 24 * 1000;
         var then = performance.now()*0.001;
-        left_lines = trial_txt_to_array(trainLeft);
-        right_lines = trial_txt_to_array(trainRight);
+        var leftTrial  = trial_txt_to_array(trainLeft);
+        var rightTrial = trial_txt_to_array(trainRight);
+        
+        // implement same trial length
+        var l = leftTrial.length - 1;
+        var r = rightTrial.length - 1;
+        if (l > r){
+           left_lines = leftTrial.slice(0, r);
+           right_lines = rightTrial.slice(0, r);
+        } else {
+            left_lines = leftTrial.slice(0, l);
+            right_lines = rightTrial.slice(0, l);
+        }
+        console.log(left_lines, right_lines);
         main(left_lines, '#glcanvas_l', then); // corrected right to left
         main(right_lines, '#glcanvas_r', then);
         jsPsych.pluginAPI.setTimeout(function() {
--- jspsych-6.1.0/plugins/turing-test.js
+++ jspsych-6.1.0/plugins/turing-test.js
@@ -23,8 +23,20 @@ jsPsych.plugins['turing-test'] = (function() {
     plugin.trial = function(display_element, trial){
         var T = Math.max(trial.stimulus.left.length, trial.stimulus.right.length) / 24 * 1000;
         var then = performance.now()*0.001;
-        left_lines = trial_txt_to_array(trial.stimulus.left);
-        right_lines = trial_txt_to_array(trial.stimulus.right);
+        var leftTrial  = trial_txt_to_array(trial.stimulus.left);
+        var rightTrial = trial_txt_to_array(trial.stimulus.right);
+    
+    // implement same trial length
+        var l = leftTrial.length - 1;
+        var r = rightTrial.length - 1;
+        if (l > r){
+           left_lines = leftTrial.slice(0, r);
+           right_lines = rightTrial.slice(0, r);
+        } else {
+            left_lines = leftTrial.slice(0, l);
+            right_lines = rightTrial.slice(0, l);
+        }
+        console.log(left_lines, right_lines);
         main(left_lines, '#glcanvas_l', then);
         main(right_lines, '#glcanvas_r', then);
         jsPsych.pluginAPI.setTimeout(function() {
